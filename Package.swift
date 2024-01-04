88c753a84a4848ba635952e66abd76b21980e122
pythonswiftlink <pythonswiftlink@gmail.com>
2024-01-04 22:52:24 +0000

    Update from https://github.com/PythonSwiftLink/KivyCoreBuilder/commit/c954a0891a28c22758f8185dc9f0a485bcb4c5f1

--- Package.swift
+++ Package.swift
@@ -54,9 +54,9 @@ let package = Package(
         	linkerSettings: [
         	]
     	), 
-    	.binaryTarget(name: "libssl", url: "https://github.com/PythonSwiftLink/KivyPythonCore/releases/download/311.0.3/libssl.zip", checksum: "351049b713ca26b0b6d1384ae6ad8737bd695657c74c3da877ac67d601a6c7fe"), 
-    	.binaryTarget(name: "libcrypto", url: "https://github.com/PythonSwiftLink/KivyPythonCore/releases/download/311.0.3/libcrypto.zip", checksum: "9883c4d905d7780bd0cebe11d9c8a2cd0f72069794d7d48ffabc1a9f71a2b778"), 
-    	.binaryTarget(name: "libpython311", url: "https://github.com/PythonSwiftLink/KivyPythonCore/releases/download/311.0.3/libpython311.zip", checksum: "024cb12b7850c374f897f162434d368780322ac32112b9bae5f743eaab2839f3"), 
-    	.binaryTarget(name: "libffi", url: "https://github.com/PythonSwiftLink/KivyPythonCore/releases/download/311.0.3/libffi.zip", checksum: "adc3cb8cd7e2e56651477d827838cd01dd35ccb3f237c7e60fba3ec65c180ed2"), 
+    	.binaryTarget(name: "libssl", url: "https://github.com/PythonSwiftLink/KivyPythonCore/releases/download/311.0.3/libssl.zip", checksum: "82d3f08840413b00fc04368c37cbef7ebc35b008305ca160c5a0e5d4be9c31f5"), 
+    	.binaryTarget(name: "libcrypto", url: "https://github.com/PythonSwiftLink/KivyPythonCore/releases/download/311.0.3/libcrypto.zip", checksum: "c97a9fea6db28344c1db947a919bcb4cc590674c73376441767131c1a60ca944"), 
+    	.binaryTarget(name: "libpython311", url: "https://github.com/PythonSwiftLink/KivyPythonCore/releases/download/311.0.3/libpython311.zip", checksum: "718b7b8c1f245ab470f98ca9bb6dfd70ce619aa5eb0ab0489e0a55aea269a43f"), 
+    	.binaryTarget(name: "libffi", url: "https://github.com/PythonSwiftLink/KivyPythonCore/releases/download/311.0.3/libffi.zip", checksum: "8b32bb6b6aab44f422e40b3320fbff2650d2e4dee6a2fa26c5a6e6646396659b"), 
     ]
 )
